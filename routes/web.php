59f3d7b7a46e4e9985b4649fa22640a53beff5af
gabrieltino <miracle8093@gmail.com>
2019-12-01 08:07:34 +0100

    changed default route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
 Route::prefix('/admin')->name('admin.')->namespace('Admin')->group(function () {
