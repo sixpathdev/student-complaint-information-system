baa509c811306f4901ecaf2f6e5608e5a40a2887
gabrieltino <miracle8093@gmail.com>
2019-12-01 22:08:26 +0100

    database details set

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'LoginController@showLoginForm');
 
 // Route::get('/', function () {
 //     return view('login');
