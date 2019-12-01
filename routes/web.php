7ca86cf22f1f8d59a72f3a735562bd94a00cbb5f
gabrieltino <miracle8093@gmail.com>
2019-12-01 08:08:56 +0100

    changed default route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
+// Route::get('/', function () {
+//     return view('login');
+// });
+
+Route::get('/', 'LoginController@showLoginForm')->name('login');
 
 Route::prefix('/admin')->name('admin.')->namespace('Admin')->group(function () {
     //All the admin routes will be defined here...
